5aedd6eeaab7630853759228ffab14113192ea4d
Prem Choudhary <pk5480524@gmail.com>
2024-09-26 22:41:01 +0530

    closure

---
+++ DSA in Interview/closure.js
@@ -0,0 +1,24 @@
+
+
+
+// function myfun(){
+//     let a = 4
+//     return a * a
+// }
+
+// console.log(myfun())
+
+// let a = 4
+// function myFun(){
+//     return a * a
+// }
+
+// console.log(myFun())
+myFun()
+function myFun(){
+    a = 4
+}
+
+
+
+
