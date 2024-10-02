5fda1b33d75b65ef538832e733a63a645937f3a4
Prem Choudhary <pk5480524@gmail.com>
2024-10-02 19:12:38 +0530

    bubble- method

--- DSAinjavascript/closure.js
+++ DSAinjavascript/closure.js
@@ -1,27 +1,9 @@
-// function myfun(){
-//     let a = 4
-//     return a * a
-// }
-
-// console.log(myfun())
-
-// let a = 4
-// function myFun(){
-//     return a * a
-// }
-
-// console.log(myFun())
-// myFun()
-// function myFun(){
-//     a = 4
-// }
-
 function selectionSort(arr) {
   let n = arr.length;
 
   for (let i = 0; i < n - 1; i++) {
     let minIndex = i;
- 
+
     for (let j = i + 1; j < n; j++) {
       if (arr[j] < arr[minIndex]) {
         minIndex = j;
@@ -38,5 +20,26 @@ function selectionSort(arr) {
   return arr;
 }
 
+let array1 = [5, 4, 1, 3, 0, 7, 8, 9];
+console.log(selectionSort(array1));
+
+//  bubble
+
+function bubbleSort(arr) {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
 let array = [5, 4, 1, 3, 0, 7, 8, 9];
-console.log(selectionSort(array));
+console.log(bubbleSort(array));
