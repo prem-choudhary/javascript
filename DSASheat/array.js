77c6414eb944d19b2ea0398adf82c73578760954
Prem Choudhary <pk5480524@gmail.com>
2024-10-04 13:48:42 +0530

    use-console

---
+++ DSASheat/array.js
@@ -0,0 +1,11 @@
+const arr = ["Apple", "Banana", "Kiwi", "Cherry"];
+console.dir(arr);
+
+//  console.table
+
+const users = [
+  { name: "Prem", age: 22 },
+  { name: "Bob", age: 10 },
+];
+
+console.table(users);
