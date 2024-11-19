c442fbce2e5fcd16573884d4b39a033f31ab9fec
Prem Choudhary <pk5480524@gmail.com>
2024-11-19 17:45:32 +0530

    odd and even

---
+++ someFile.js/index.js
@@ -0,0 +1,10 @@
+console.log("log")
+
+
+const n = 20
+for(let i = 0 ; i<=n ; i++){
+    if (i % 2 == 0){
+        console.log( i , "Number is even")
+    }else if(i % 2 == 1)
+        console.log(i, "Number is odd")
+    }
