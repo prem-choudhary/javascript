ee4191d886611ac5321fb013503325967172e096
Prem-choudhary <pk5480524@gmail.com>
2025-05-31 12:40:33 +0530

    object

---
+++ javascript-interview/object.js
@@ -0,0 +1,11 @@
+const obj = { a: 1, b: 2, c: 3 };
+// Object.keys() returns an array of the object's own enumerable property names
+console.log(Object.keys(obj));
+// Object.values() returns an array of the object's own enumerable property values
+console.log(Object.values(obj));
+console.log(Object.entries(obj));
+// Object.entries() returns an array of the object's own enumerable property [key, value] pairs
+const entries = Object.entries(obj);
+
+// Object.fromEntries() transforms a list of key-value pairs into an object
+const newObj = Object.fromEntries(entries);
