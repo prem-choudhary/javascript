6c3ba8e009adf4222b9de3a52e705fccf9a8a620
Prem-choudhary <pk5480524@gmail.com>
2025-06-04 10:45:41 +0530

    obj

--- javascript-interview/obj.js
+++ javascript-interview/obj.js
@@ -4,4 +4,6 @@ const source = { b: 2, c: 3 };
 const result = Object.assign(target, source);
 
 console.log(result); // { a: 2, b: 2, c: 3 }
-console.log(target);
+console.log(target);    
+// { a: 2, b: 2, c: 3 }
+
