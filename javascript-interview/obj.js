9e9df4107cad4552f5f52ac447ec9c8f2713e2de
Prem-choudhary <pk5480524@gmail.com>
2025-06-06 10:58:39 +0530

    object

--- javascript-interview/obj.js
+++ javascript-interview/obj.js
@@ -6,4 +6,6 @@ const result = Object.assign(target, source);
 console.log(result); // { a: 2, b: 2, c: 3 }
 console.log(target);    
 // { a: 2, b: 2, c: 3 }
+// The target object is modified, and now it contains properties from both target and source
+console.log(source);
 
