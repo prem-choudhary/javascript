da2281e55e961d4c963c3c11e18fe8caa7bc5ee9
Prem-choudhary <pk5480524@gmail.com>
2025-06-08 08:41:49 +0530

    new-change

--- javascript-interview/obj.js
+++ javascript-interview/obj.js
@@ -3,6 +3,7 @@ const source = { b: 2, c: 3 };
 // Using Object.assign to merge properties from source into target
 const result = Object.assign(target, source);
 
+
 console.log(result); // { a: 2, b: 2, c: 3 }
 console.log(target);    
 // { a: 2, b: 2, c: 3 }
