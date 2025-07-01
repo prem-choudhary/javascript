773e87b9e382a947c54e1d04a987bd2f011dd8ca
Prem-choudhary <pk5480524@gmail.com>
2025-07-01 19:05:35 +0530

    array-simple code

--- javascript-interview/basicCode.js
+++ javascript-interview/basicCode.js
@@ -3,6 +3,10 @@ const num = 1;
 console.log(str == num);
 console.log(str === num);
 
-//  add some basic code  
+//  add some basic code
 
-console.log([] == ![])
+console.log([] == ![]);
+
+const magic = [3, 2, 1] + [1, 2, 3];
+
+console.log(magic); // out 3,2,11,2,3
