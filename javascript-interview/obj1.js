55bf295253b8f7c483907aa865a0ac53feffa83d
Prem-choudhary <pk5480524@gmail.com>
2025-07-07 09:06:39 +0530

    save-obj

--- javascript-interview/obj1.js
+++ javascript-interview/obj1.js
@@ -14,4 +14,4 @@ console.log("Map: ", map);
 console.log("Object length:", Object.keys(obj).length); // 3
 console.log("Map size:", map.size); // 3
 
-console.log(0.2+ 0.2 === 0.3);
+console.log(0.2 + 0.2 === 0.3);
