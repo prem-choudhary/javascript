689c4d39f90d66aa0107515e21d9b8b008261fa8
Prem-choudhary <pk5480524@gmail.com>
2025-07-17 10:06:49 +0530

    Global Variable

---
+++ javascript-interview/day1.js
@@ -0,0 +1,10 @@
+let petName = "Rocky"; // Global Variable
+myFunction();
+
+function myFunction() {
+  console.log("Inside myFunction - Type of petName:", typeof petName);
+  console.log("Inside myFunction - petName:", petName);
+}
+
+console.log("Outside myFunction - Type of petName:", typeof petName);
+console.log("Outside myFunction - petName:", petName);
