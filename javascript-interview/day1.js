07a56a9697a50d86a2a3bd7505f722d65cc75c3e
Prem-choudhary <pk5480524@gmail.com>
2025-07-19 09:15:13 +0530

    some Change

--- javascript-interview/day1.js
+++ javascript-interview/day1.js
@@ -4,7 +4,6 @@ myFunction();
 function myFunction() {
   console.log("Inside myFunction - Type of petName:", typeof petName);
   console.log("Inside myFunction - petName:", petName);
-  
 }
 
 console.log("Outside myFunction - Type of petName:", typeof petName);
