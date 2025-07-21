dfbfa3c5d418ba9fe5b8c1d6b44fb86e745d23c6
Prem-choudhary <pk5480524@gmail.com>
2025-07-21 14:09:38 +0530

    Function

---
+++ javascript-interview/Function01.js
@@ -0,0 +1,42 @@
+function reverseMyWay(array) {
+  var output = [];
+  for (var i = array.length - 1; i > -1; i--) {
+    output.push(array[i]);
+  }
+
+  return output;
+}
+
+function reverseTheirWay(array) {
+  var output = [];
+  while (array.length) {
+    output.push(array.pop());
+  }
+
+  return output;
+}
+
+function JustDoIt() {
+  console.log("their way starts");
+  var startOf = new Date().getTime();
+  for (var p = 0; p < 10000; p++) {
+    console.log(reverseTheirWay([7, 6, 5, 4, 3, 2, 1]));
+  }
+  var endOf = new Date().getTime();
+  console.log("ran for " + (endOf - startOf) + " ms");
+  console.log("their way ends");
+}
+
+function JustDoIMyWay() {
+  console.log("my way starts");
+  var startOf = new Date().getTime();
+  for (var p = 0; p < 10000; p++) {
+    console.log(reverseMyWay([7, 6, 5, 4, 3, 2, 1]));
+  }
+  var endOf = new Date().getTime();
+  console.log("ran for " + (endOf - startOf) + " ms");
+  console.log("my way ends");
+}
+
+JustDoIt();
+JustDoIMyWay();
