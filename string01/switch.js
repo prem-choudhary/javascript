e55aee6a8750e9b30f86fb0dc13cc636b6c24afd
Prem-choudhary <pk5480524@gmail.com>
2025-11-13 15:12:41 +0530

    switch-case in javascript

---
+++ string01/switch.js
@@ -0,0 +1,53 @@
+// //   switch (key) {
+// //     case value:
+
+// //         break;
+
+// //     default:
+// //         break;
+// //   }
+
+const month = 5;
+
+switch (month) {
+  case 1:
+    console.log("januray");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("may");
+    break;
+  case 6:
+    console.log("june");
+    break;
+  case 7:
+    console.log("jul");
+    break;
+  case 8:
+    console.log("aug");
+    break;
+  case 9:
+    console.log("sep");
+    break;
+  case 10:
+    console.log("oct");
+    break;
+  case 11:
+    console.log("nov");
+    break;
+  case 12:
+    console.log("dec");
+    break;
+
+  default:
+    console.log("default case ");
+    break;
+}
