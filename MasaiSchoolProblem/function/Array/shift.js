1b40c55c9147335d26313f9187dba93a8def0bda
Prem-choudhary <pk5480524@gmail.com>
2025-11-22 16:02:59 +0530

    Array methods

---
+++ MasaiSchoolProblem/function/Array/shift.js
@@ -0,0 +1,16 @@
+// const array = [1, 2, 3];
+// console.log(array)
+// const firstElement = array.shift();
+
+// console.log(array);
+// // Expected output: Array [2, 3]
+
+// console.log(firstElement);
+// // Expected output: 1
+// console.log(array)
+
+const array = [1, 2, 3, 4];
+console.log(array);
+const removedElement = array.shift();
+console.log(removedElement," this is removed from array list");
+console.log(array);
