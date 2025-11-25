1bac8dc5f7d6ca06615a5b77b8a9cb9ef356e11b
Prem-choudhary <pk5480524@gmail.com>
2025-11-25 14:36:53 +0530

    object

---
+++ DSASheat/objec1.js
@@ -0,0 +1,20 @@
+var student = {
+  name: "Praveen",
+  age: 24,
+  //   city: Surat,
+  gender: "Male",
+  marks: {
+    Maths: 45,
+    english: 41,
+    science: 48,
+    history: 44,
+    geography: 40,
+  },
+  address: {
+    city: "Surat",
+    pineCode: "0001",
+  },
+};
+
+console.log(student.marks);
+console.log(student.address.city);
