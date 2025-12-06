927ed5d486df39cf08a0b705d7a89d60a6d76ce5
Prem-choudhary <pk5480524@gmail.com>
2025-12-06 13:23:59 +0530

    destructuring to take out each language into separate variable:

---
+++ multiDArray/javascript.js
@@ -0,0 +1,13 @@
+// list of languages
+
+let pLang = ["Java", "JavaScript", "Python"];
+
+// using destructuring to take out each language into separate variable:
+
+let [pLang1, pLang2, pLang3] = pLang;
+
+console.log(pLang1); // Java
+
+console.log(pLang2); // JavaScript
+
+console.log(pLang3); // Python
