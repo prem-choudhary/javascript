6a54a7f134673123a7a0cf87a72c593dae877255
Prem-choudhary <pk5480524@gmail.com>
2025-12-15 14:35:34 +0530

    Array

--- multiDArray/index.js
+++ multiDArray/index.js
@@ -3,28 +3,21 @@ const arr = [
   [1, 2, 3, 4, 5],
   ["1", 2, 3, 4, "5"],
 ];
-
 console.log(arr[0]);
 console.log(arr[1]);
 console.log(arr[2]);
-
 const el = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
 ];
-
 console.log(el.length);
-
 let arr1 = [];
 const polythene1 = ["tomato1", "tomato2", "tomato3"];
 const polythene2 = ["onion1", "onion2", "onion3"];
-
 arr1.push(polythene1);
-
 arr1.push(polythene2);
 console.log(arr1);
-
 let pattern = [];
 const bag = ["*", "*", "*"];
 const bag1 = ["#", "#"];
