12366b57c9c607ad13db7a4637c4364165f31ade
Prem-choudhary <pk5480524@gmail.com>
2026-02-01 12:51:05 +0530

    loop in javascript

--- challenges/loop.js
+++ challenges/loop.js
@@ -34,8 +34,6 @@ if (num > 0) {
   console.log("number is negative");
 }
 
-// Ticket Pricing
-// Write a program that calculates the ticket price based on age with the following conditions: age below 12 pay a ticket price of //   5,  age below 18 pay a ticket price of 10, age below 60 pay a ticket price of 20, age over 60 play a ticket price of 15.
 
 let age = 69;
 let ticketPrice;
