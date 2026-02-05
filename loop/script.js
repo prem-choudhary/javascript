5561e94753a3ed09041ca5184707396c0e569a68
Prem-choudhary <pk5480524@gmail.com>
2026-02-05 10:08:12 +0530

    understand loop work

--- loop/script.js
+++ loop/script.js
@@ -1,1 +1,24 @@
-console.log("Hello user")
+console.log("Hello user");
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
+for (let j = 0; j < 5; j++) {
+  console.log(j);
+}
+let k = 0;
+do {
+  console.log(k);
+  k++;
+} while (k < 5);
+let arr = [1, 2, 3, 4, 5];
+for (let index in arr) {
+  console.log(index);
+}
+for (let value of arr) {
+  console.log(value);
+}
+arr.forEach((value) => {
+  console.log(value);
+});
