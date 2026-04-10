e92398a8dc15f27eae058222cded84cb010e12dc
Prem-choudhary <pk5480524@gmail.com>
2026-04-10 10:46:09 +0530

    JavaScript code

--- javascript-interview/arrayfun.js
+++ javascript-interview/arrayfun.js
@@ -12,6 +12,7 @@ function binaryMatrix(N, M, matrix) {
     console.log(jhola);
   }
 }
+
 function binaryMatrix(N, M, matrix) {
   // Validate input
   if (
@@ -22,6 +23,7 @@ function binaryMatrix(N, M, matrix) {
     console.log("Invalid matrix dimensions");
     return;
   }
+
   // Process each row
   for (let i = 0; i < N; i++) {
     let rowString = "";
@@ -36,6 +38,7 @@ function binaryMatrix(N, M, matrix) {
     console.log(rowString); 
   }
 }
+
 console.log(
   binaryMatrix(3, 3, [
     [1, 0, 1],
