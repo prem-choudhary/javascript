ea90ed4733c269eedb4cdc88628e931600daeedb
Prem-choudhary <pk5480524@gmail.com>
2026-05-01 15:30:55 +0530

    push code on github

---
+++ javascript-interview/javascriptQuestion.js
@@ -0,0 +1,31 @@
+const Person = function (name) {
+  this.firstName = name;
+  this.sayName1 = function () {
+    console.log(this.firstName);
+  };
+  this.sayName2 = () => {
+    console.log(this.firstName);
+  };
+};
+
+const john = new Person('John');
+const dave = new Person('Dave');
+
+john.sayName1(); // John
+john.sayName2(); // John
+
+// The regular function can have its `this` value changed, but the arrow function cannot
+john.sayName1.call(dave); // Dave (because `this` is now the dave object)
+john.sayName2.call(dave); // John
+
+john.sayName1.apply(dave); // Dave (because `this` is now the dave object)
+john.sayName2.apply(dave); // John
+
+john.sayName1.bind(dave)(); // Dave (because `this` is now the dave object)
+john.sayName2.bind(dave)(); // John
+
+const sayNameFromWindow1 = john.sayName1;
+sayNameFromWindow1(); // undefined (because `this` is now the window object)
+
+const sayNameFromWindow2 = john.sayName2;
+sayNameFromWindow2(); // John
