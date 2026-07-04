1592054330bbb9167f6bcd9c62cf792c34b24f7a
Prem-choudhary <pk5480524@gmail.com>
2026-07-04 10:15:09 +0530

    code change in this file

--- javascript-interview/DSA4.js
+++ javascript-interview/DSA4.js
@@ -13,14 +13,13 @@ function sumNested(arr) {
 
   return total;
 }
-
 // Test cases
 // console.log(sumNested([1, [2, 3], [4, [5]]))); // 15
 console.log(
   sumNested([
     [1, 2],
     [3, [4, 5]],
-  ])
+  ]),
 ); // 15
 console.log(sumNested([[1], [2]])); // 3
 
