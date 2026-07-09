fb3aea135c8ce87a194658e5ffba7175c437d636
Prem-choudhary <pk5480524@gmail.com>
2026-07-09 09:18:57 +0530

    some change

--- javascript-interview/day1.js
+++ javascript-interview/day1.js
@@ -1,11 +1,8 @@
 let petName = "Rocky"; // Global Variable
 myFunction();
-
 function myFunction() {
   console.log("Inside myFunction - Type of petName:", typeof petName);
   console.log("Inside myFunction - petName:", petName);
 }
-
 console.log("Outside myFunction - Type of petName:", typeof petName);
 console.log("Outside myFunction - petName:", petName);
-
