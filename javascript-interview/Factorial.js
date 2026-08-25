d688939ded2fa56f192225ded7d851abd794f637
Prem-choudhary <pk5480524@gmail.com>
2026-08-25 08:53:14 +0530

    some change in this code

--- javascript-interview/Factorial.js
+++ javascript-interview/Factorial.js
@@ -4,13 +4,5 @@ let fact = 1;
 for (let i = 1; i <= n; i++) {
   fact *= i;
 }   
-console.log(fact); // 120
-
-/**
- 1 * 1 = 1
-1 * 2 = 2
-2 * 3 = 6
-6 * 4 = 24
-24 * 5 = 120
-
- */
+console.log(fact); 
+// Output: 120
