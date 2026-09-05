ad359bcaf471b642688540e52508aff6fce38862
Prem-choudhary <pk5480524@gmail.com>
2026-09-05 09:32:16 +0530

    new change  in this code

--- javascript-interview/array.js
+++ javascript-interview/array.js
@@ -3,7 +3,6 @@ let fruits = ["apple", "banana", "mango"];
 
 // Using the Array constructor
 let numbers = new Array(1, 2, 3, 4);
-
 console.log(fruits[0]); // "apple"
 console.log(fruits[1]) // "banana"
 console.log(fruits[2]); // "mango"
