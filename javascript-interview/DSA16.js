85926c2af2b6a405f01f8f1fcc47bce9104fc3bc
Prem-choudhary <pk5480524@gmail.com>
2026-09-19 09:15:13 +0530

    DSA Problem

--- javascript-interview/DSA16.js
+++ javascript-interview/DSA16.js
@@ -2,19 +2,16 @@ class Stack {
   constructor() {
     this.items = [];
   }
-
   // Push an element onto the stack
   push(element) {
     this.items.push(element);
   }
-
   // Pop an element from the stack
   pop() {
     if (this.isEmpty()) return null;
 
     return this.items.pop();
   }
-
   // Peek at the top element
   peek() {
     if (this.isEmpty()) return null;
